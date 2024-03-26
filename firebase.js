05e5c6c01f49c3a7183ab751d7672af7efa21f51
Pongki69 <alfonsopanisales69420@gmail.com>
2024-03-26 12:04:33 +0800

    First Commit

---
+++ firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcvXZdpPc7-cQ6ZAgmux4pvYCLnE0Ze4I",
+  authDomain: "job-offer-1b71e.firebaseapp.com",
+  projectId: "job-offer-1b71e",
+  storageBucket: "job-offer-1b71e.appspot.com",
+  messagingSenderId: "890055227250",
+  appId: "1:890055227250:web:77bbac8bfb45cdb984cfeb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
